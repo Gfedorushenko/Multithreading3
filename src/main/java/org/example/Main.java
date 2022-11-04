73336fe19225f9e109ce13f366a78f449c642bd6
Григорий Федорущенко <Gfedorushenko@mail.ru>
2022-11-04 23:10:15 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,88 @@
+package org.example;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    static AtomicInteger atomicInt3 = new AtomicInteger(0);
+    static AtomicInteger atomicInt4 = new AtomicInteger(0);
+    static AtomicInteger atomicInt5 = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        String[] texts = new String[100_000];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+
+        Runnable logic1 = () -> {
+            for (int i = 0; i < texts.length; i++) {
+                if (lettersInAscendingOrder(texts[i])) {
+                    beautifulWord(texts[i]);
+                }
+            }
+        };
+        Runnable logic2 = () -> {
+            for (int i = 0; i < texts.length; i++) {
+                if (isPalindrome(texts[i])) {
+                    beautifulWord(texts[i]);
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(logic1);
+        thread1.start();
+        Thread thread2 = new Thread(logic2);
+        thread2.start();
+
+        //Слова состоящие из одной и той же буквы (критерий 2) подходят под условие палиндрома (критерий 1).
+        // Для получения правильных результатов проверку по критерию 2 делать не стал иначе будет задвоение.
+
+        thread1.join();
+        thread2.join();
+        System.out.println("Красивых слов с длиной 3: " + atomicInt3.get() + " шт");
+        System.out.println("Красивых слов с длиной 4: " + atomicInt4.get() + " шт");
+        System.out.println("Красивых слов с длиной 5: " + atomicInt5.get() + " шт");
+    }
+
+    public static void beautifulWord(String s) {
+        if (s.length() == 3)
+            atomicInt3.incrementAndGet();
+        else if (s.length() == 4)
+            atomicInt4.incrementAndGet();
+        else if (s.length() == 5)
+            atomicInt5.incrementAndGet();
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
+    public static boolean isPalindrome(String s) {
+        int n = s.length();
+        for (int i = 0; i < (n / 2); ++i) {
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean lettersInAscendingOrder(String s) {
+        int n = s.length();
+        if (s.charAt(0) != s.charAt(n - 1)) {
+            for (int i = 0; i < s.length() - 1; ++i) {
+                if ((int) s.charAt(i) > (int) s.charAt(i + 1)) {
+                    return false;
+                }
+            }
+            return true;
+        } else
+            return false;
+    }
+}
